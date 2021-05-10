5d7316c6d89aedbd3a950df83589628d95ed248e
Gustavo <gustavo.gigante.s@gmail.com>
2021-05-10 22:45:59 +0100

    v1

---
+++ index.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+const config = require("./config.json");
+const client = new Discord.Client();
+const quotes = [
+    {by: 'John Johnson', desc:'First, solve the problem. Then, write the code.'},
+    {by: 'Rob Castanheira', desc:'Vai trabalhar, monte de merda.'},
+    {by: 'Everyone', desc:'Console.log is the best debugger'},
+    {by: 'ImInYourFino', desc:'Breaking doors is the best medicine.'},
+    {by: 'Mãe tuga', desc:'Come ou levas com o chinelo.'},
+    {by: 'Rob Castanheira', desc:'Getting beaten up makes me feel alive'},
+    {by: 'Luna', desc:'Garrosh did nothing wrong. You did.'}
+]
+
+const prefix = "!";
+client.on("message", function(message) { 
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+
+    const commandBody = message.content.slice(prefix.length);
+    const args = commandBody.split(' ');
+    const command = args.shift().toLowerCase();
+    
+  if (command === "quote") {
+      const randomQuote = quotes[Math.floor(Math.random() * quotes.length)]
+    const toSend = `"${randomQuote.desc}" - ${randomQuote.by}`;
+    message.reply(toSend);
+    }  
+});  
+
+client.login(config.BOT_TOKEN);
