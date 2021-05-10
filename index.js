db5990db9f4d18da855be71d9b88e03dd5954317
Gustavo <gustavo.gigante.s@gmail.com>
2021-05-10 23:21:34 +0100

    test command list

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const quotes = [
     {by: 'Luna', desc:'Garrosh did nothing wrong. You did.'}
 ]
 
-const commandList = ['?quotes']
+const commandList = ['"?quotes"', '"test"']
 
 const prefix = "?";
 client.on("message", function(message) { 
